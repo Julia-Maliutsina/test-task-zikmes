f6c963f98c5ef4cdc3f0f180cdc3a397af3e1222
Julia <julia.maliutsina@gmail.com>
2022-11-16 18:18:09 +0300

    configure webpack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+let submit = document.getElementById('number-submit');
+let numberInput = document.getElementById('number');
+submit.addEventListener('click', () => validateInput(numberInput.value));
+
 const validateInput = (value) => {
   let error = document.getElementsByClassName('input-error')[0];
   if (!value) {
